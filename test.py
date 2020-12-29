7e555492315c9773ffb6a3ef5c2d0866aef2f471
Jose Bolanos <bolanosjjr@gmail.com>
2020-12-28 20:44:07 -0600

    some testing edits

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from app import app
 from models import db, User, Post
 
 # Use test database and don't clutter tests with SQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
 db.drop_all()
@@ -18,32 +18,102 @@ class UserTestCase(TestCase):
 
         User.query.delete()
 
+        user = User(
+            first_name="zoro", 
+            last_name="enma",
+            image_url="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/roronoa-zoro-darko-simple-art.jpg")
+
+        db.session.add(user)
+        db.session.commit()
+
+        
+        self.user_id = user.id
+
     def tearDown(self):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
 
-    def test_create(self):
+    def test_users(self):
         with app.test_client() as client:
 
-            resp = client.get('/users/new', query_string={
-            "first_name":"zoro", 
-            "last_name":"enma",
-            "image_url":"https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/roronoa-zoro-darko-simple-art.jpg"})
-        
-            self.assertIn(b"zoro", resp.data)
+            resp = client.get('/users')
+            html = resp.get_data(as_text=True)
 
-# class PostTestCase(TestCase):
-#     '''test post models'''
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('zoro',html)
 
-#     def setUp(self):
-#         """Clean up any existing pets."""
+    def test_show_user(self):
+        with app.test_client() as client:
 
-#         Post.query.delete()
+            resp = client.get(f'/users/{self.user_id}')
+            html = resp.get_data(as_text=True)
 
-#     def tearDown(self):
-#         """Clean up any fouled transaction."""
+            self.assertIn('<h1>zoro enma</h1>', html)
 
-#         db.session.rollback()
 
-#     def
+class PostTestCase(TestCase):
+    '''test post models'''
+
+    def setUp(self):
+        """Clean up any existing pets."""
+
+        Post.query.delete()
+
+        new_post = Post(
+            title="legs", 
+            content="need to run")
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        self.post_id = post.id
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+
+    def new_post(self):
+        with app.test_client() as client:
+            
+            d = {"title":"legs2","content":"did it work"}
+            resp = client.post('/users/{user.id}/posts/new', data = d, follow_redirects=True)
+
+
+            self.assertIn('legs2', html)
+    
+class TagTestcase(TestCase):
+
+    def setUp(self):
+        """Clean up any existing pets."""
+
+        Tag.query.delete()
+
+        new_tag = Tag(
+            name="runss")
+
+        db.session.add(new_tag)
+        db.session.commit()
+
+        self.tag_id = tag.id
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+
+    def new_tag(self):
+        with app.test_client() as client:
+           
+            d = {"name":"runssss"}
+            resp = client.post('/tags', data = d, follow_redirects=True)
+
+
+            self.assertEqual('runssss', html)
+
+
+
+
+
+            
